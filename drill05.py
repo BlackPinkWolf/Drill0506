d0215319071ba75164fae05de867d381a314b0b8
HyeonSang Cho <wolf1323@naver.com>
2023-10-03 10:02:22 +0900

    반복적으로 만들어지는 랜덤 손 화살표 위치로 캐릭터 이동 구현 완료

--- drill05.py
+++ drill05.py
@@ -35,12 +35,15 @@ def reset_world():
     frame = 0
     action = 3
 
+    set_new_target_arrow()
+
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
     sx, sy = cx, cy
     # hx, hy = 50, 50
-    hx, hy = random.randint(0,TUK_WIDTH), random.randint(0,TUK_HEIGHT)
+    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
     t = 0.0
-
-
 
 
 def render_world():
@@ -59,11 +62,13 @@ def update_world():
     frame = (frame + 1) % 8
     action = 1 if cx < hx else 0
 
-
     if t <= 1.0:
         cx = (1 - t) * sx + t * hx
         cy = (1 - t) * sy + t * hy
         t += 0.001
+    else:
+        cx, cy = hx, hy
+        set_new_target_arrow()
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
